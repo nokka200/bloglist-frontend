6af8cb05c7b5bceb6a505cdc51e28c7c055b3c98
Niku <nokka200@gmail.com>
2024-07-19 10:58:01 +0300

    Aloitettu tehtävä 5.1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
@@ -18,40 +19,23 @@ const App = () => {
     event.preventDefault()
   }
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
-  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user && <LoginForm
+        user={user}
+        handleLogin={handleLogin}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword} />}
+
+
+      {user && (
+        <>
+          <h2>blogs</h2>
+          {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+        </>
       )}
     </div>
   )
