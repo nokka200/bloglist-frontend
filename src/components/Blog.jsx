c00e81d8a005df061185ba386f3e8c53df40f3a2
Niku <nokka200@gmail.com>
2024-07-24 10:51:22 +0300

    5.7. valmis

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: visible ? '' : 'none'
+  }
 
   return (
-    <div>
-      {blog.title} {blog.author}
-    </div>
+    <>
+      <div style={hideWhenVisible}>
+        {blog.title}<button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={blogStyle}>
+        <div>{blog.title}<button onClick={toggleVisibility}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.author}</div>
+      </div>
+    </>
   )
 }
 
